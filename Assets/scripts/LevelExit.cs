b583b74657747d42f5d0d76337dec5b242fa8230
Katie Alberda <112493386+Alberda-Katie-Student@users.noreply.github.com>
2023-05-17 09:56:30 -0400

    Coin PIckups

---
+++ Assets/scripts/LevelExit.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] float levelLoadDelay = 1f;
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        int currectSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currectSceneIndex +1);
+        StartCoroutine(LoadNextLevel());
+       
+    }
+    IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSecondsRealtime(levelLoadDelay);
+        int currectSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currectSceneIndex +1);
+
+    }
+
+
+}
